8608deccd48a16b583195d78a19b555db886c1a6
Nathalie Bonami <nat.geyzen@outlook.com>
2020-03-30 10:21:56 -0500

    Reducers combined and any reference to state adjusted to reference correct reducer states

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -95,7 +95,6 @@ class ContactData extends Component {
                 value: 'fastest',
                 validation: {},
                 valid: true
-
             }
         }
     }
@@ -190,9 +189,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 }
 
